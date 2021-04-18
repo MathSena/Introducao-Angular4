d76f997c644823708b7400289e34266fb36d9c3f
MathSena <MathSena07@hotmail.com>
2021-04-18 13:05:47 -0300

    Criando um Mock de objetos

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,9 @@
+import {Frase} from '../shared/frase.model'
+
+const FRASES: Frase[]= [
+    {fraseEng: "You say goodbye and I say hello, Hello hello, I don't know why you say goodbye,", frasePtBr: 'Eu digo adeus e você oi, Olá,Olá, eu não sei como você diz adeus'},
+    {fraseEng: "I know I'll often stop and think about them, In my life I'll love you more", frasePtBr: 'Eu sei que sempre vou parar e pensar neles, na minha vida, te amarei mais'},
+    {fraseEng: 'I wanna hold your hand', frasePtBr: 'Eu quero segurar a sua mão'},
+    {fraseEng: "Here comes the sun, here comes the sun, It's all right", frasePtBr: 'Lá vem o sol, lá vem o sol, e está tudo bem'},
+    {fraseEng: "Hey, Jude, don't make it bad, Take a sad song and make it better", frasePtBr: 'Hey Jude, não piore as coisas, pegue uma canção triste e faça melhor'}
+]
